f281dd4eaa823f159def21ecb7cda20507452064
Taylor Smith <taylor.l.smith07@gmail.com>
2022-09-29 18:11:13 -0700

    refactored breedFetcher and created mocha chai test cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,21 @@
 const request = require('request');
-const breed = process.argv[2];
 
-
-
-request(`https://api.thecatap.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    if (error.code === "ENOTFOUND") {
-      console.log("Error:", "Breed name not found");
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
       return;
     }
-    console.log(error);
-    return;
-  }
-  const data = JSON.parse(body);
-  console.log(data[0].description);
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback("error: Breed name not found", null);
+      return;
+    }
+    const desc = data[0].description;
+    callback(null, desc);
   });
+};
+
+module.exports = {
+  fetchBreedDescription
+};
